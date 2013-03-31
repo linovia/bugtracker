2426d782553b115713ab7b0975c9b852832e875f
Xavier Ordoquy <xordoquy@linovia.com>
2013-03-31 18:17:01 +0200

    Absolute database file and static/media configuration

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -1,7 +1,11 @@
 # Django settings for bugtracker project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_ROOT = os.path.basename(__file__)
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -12,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'bugtracker.db',
+        'NAME': os.path.join(PROJECT_ROOT, '..', 'bugtracker.db'),
     }
 }
 
@@ -43,23 +47,10 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, '..', 'media')
+MEDIA_URL = '/media/'
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'static')
 STATIC_URL = '/static/'
 
 # Additional locations of static files
